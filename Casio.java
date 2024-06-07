d458dcb212ce1f01f9cf9996934e1b1ff6f2bf41
cse20at1a0594 <124234289+cse20at1a0594@users.noreply.github.com>
2024-06-08 00:54:18 +0530

    Add files via upload

---
+++ Casio.java
@@ -0,0 +1,75 @@
+import java.util.*;
+class Casio{
+public static void main(String arg[]){
+Scanner s=new Scanner(System.in);
+System.out.println("STUDENT DETAILS");
+String name,branch;
+
+System.out.println("Student name: ");
+name=s.next();
+System.out.println("Student branch: ");
+branch=s.next();
+
+int amt,rollno;
+System.out.println("Student Amt: ");
+amt=s.nextInt();
+System.out.println("Student roll: ");
+rollno=s.nextInt();
+System.out.println("\nACCOUNTER DETAILS");
+String Accname;
+System.out.println("Acc name: ");
+Accname=s.next();
+int Accsal;
+System.out.println("Acc sal: ");
+Accsal=s.nextInt();
+System.out.println("\nCANTEEN DETAILS");
+String Ownername,drinks,meals;
+ int price,selleramt;
+ System.out.println("Name Of The Seller: ");
+Ownername=s.next();
+ System.out.println("Name Of Drinks: ");
+drinks=s.next();
+  System.out.println("Name Of Meals: ");
+meals=s.next();
+     System.out.println("Person Income: ");
+selleramt=s.nextInt();
+         System.out.println("Total Amount: ");
+price=s.nextInt();
+System.out.println("\nSTATIONARY DETAILS");
+ String personname,books,pens;
+ 
+System.out.println("Seller name: ");
+personname=s.next();
+        
+         System.out.println("book name: ");
+books=s.next();
+         System.out.println("pen name: ");
+pens=s.next();
+int pamt,book,pen;
+         System.out.println("no of books student want to purhase: ");
+book=s.nextInt();
+         System.out.println("no of pens student want to purhase: ");
+pen=s.nextInt();
+ System.out.println("Total amount of shop owner: ");
+pamt=s.nextInt();
+System.out.println("\nLIBRARYDETAILS");
+String Manager,Author,Bookname;
+    
+  
+     System.out.println("Name Of Manager: ");
+Manager=s.next();
+System.out.println("Name Of Author: ");
+Author=s.next();
+     System.out.println("Book Name: ");
+Bookname=s.next();
+
+int Manageramt,Edition;
+     System.out.println("Edition No: ");
+
+Edition=s.nextInt();  
+     System.out.println("Amt: ");  
+Manageramt=s.nextInt(); 
+
+}}
+
+ 
